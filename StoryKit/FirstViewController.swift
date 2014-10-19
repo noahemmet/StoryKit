c6abd75babc0f3a57d3d06e739ece6af4db12a2f
Noah Emmet <noah@noahemmet.com>
2014-10-19 17:08:35 -0500

    Initial Commit

---
+++ StoryKit/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  StoryKit
+//
+//  Created by Noah Emmet on 10/19/14.
+//  Copyright (c) 2014 Sticks. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
